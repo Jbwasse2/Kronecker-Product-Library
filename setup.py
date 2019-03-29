ded61cf6b38138a162ba3dfae3c03a35845341fe
JustinW <jbwasse2@illinois.edu>
2019-03-28 20:56:03 -0500

    Got rid of examples, will just be used in documentation instead

--- setup.py
+++ setup.py
@@ -2,19 +2,17 @@
 from setuptools import setup, find_packages
 
 setup(name="kronprod",
-      version="1.0.dev0",
-      author="Tauhid",
-      url="https://github.com/tauhid03/MarkovChainAnalysis",
-      description="Markov Chain Analysis",
+      version="1.0",
+      author="Justin Wasserman, Alexandra Nilles",
+      url="https://github.com/Jbwasse2/Kronecker-Product-Library",
+      description="Kronecker Product Library",
       long_description=" ",
       classifiers=[
           "Programming Language :: Python",
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.2",
-          "Programming Language :: Python :: 3.3",
           "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
           "Topic :: Scientific/Engineering",
           "Topic :: Scientific/Engineering :: Mathematics",
           "Topic :: Software Development :: Libraries :: Python Modules"],
@@ -22,4 +20,4 @@ setup(name="kronprod",
       license="MIT",
       py_modules=['kronprod', 'KronMDP'],
       packages=find_packages(),
-      install_requires=["numpy", "scipy", "pymdptoolbox"])
+      install_requires=["numpy", "scipy"])
